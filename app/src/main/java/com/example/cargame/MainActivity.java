69aed0073446285f3d2433dca1bb5f56cfdfb6b5
Jona Goeltenboth <jona.goeltenboth@fh-aachen.de>
2023-03-22 11:36:06 +0100

    removed unneccessary imports

--- app/src/main/java/com/example/cargame/MainActivity.java
+++ app/src/main/java/com/example/cargame/MainActivity.java
@@ -1,22 +1,10 @@
 package com.example.cargame;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ImageView auto1;
-    private ImageView auto2;
-    private ViewGroup.LayoutParams layout;
-    private ImageView original;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
